22d857a554e851d4c882eda9c69da1c5569afb32
nserpa24 <31513107+nserpa24@users.noreply.github.com>
2020-05-17 16:54:11 -0400

    Update venty_copy.ino
    Further refinements by Phil M. (added comments and removing dead code)

--- venty_copy.ino
+++ venty_copy.ino
@@ -1,12 +1,26 @@
+// Code for MakeIt Venty
+// by Nick Serpa, Jonathan Vail, Phil Martel
+// License ??
+/* Bried description:
+ *  Code for an Arduino driven AMBU_based ventilator
+ *  based on the MIT e-vent https://e-vent.mit.edu/
+ */
+ /* TO-DO:
+  *  1 - Button board fix - One increment/decrement of BPM, Tidal, and I/E per button push (Phil Completed)
+  *  2 - Pressure sensor code - Set off a high or low alarm, will likely output to LED
+  *  3 - Initialization of the arm by having it contact the upper limit switch, setting position to zero, and waiting for sin function to match before  enabling PID driven motion
+  *  4 - Sin wave function optimization - Create the data for each BPM and Tidal volume option and integrate
+  *  5 - Limit the tidal and BPM values as well as scroll through available options for I:E
+  */
+  
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Encoder.h>
-
 boolean toggle4 = LOW;
 unsigned long tick = 0;
 unsigned long mtime;
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 11; // Analog output pin that the LED is attached to
+// not used.  It's called MOTORPIN.  const int analogOutPin = 11; // Analog output pin that the LED is attached to
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
 int speed;
@@ -18,64 +32,56 @@ long state = 0;
 int bpm = 10;
 int tidalvol = 400;
 int ieratio =1;
+// making the pins const int
 boolean start = LOW;
-int kpin = 10;
+const int kpin = 10;
 boolean kstate = 0;
-int lpin = 9;
+const int lpin = 9;
 boolean lstate = 0;
-int mpin = 8;
+const int mpin = 8;
 boolean mstate = 0;
-int npin = 6;
+const int npin = 6;
 boolean nstate = 0;
-int spin = 5;
+const int spin = 5;
 boolean sstate = 0;
-int tpin = 4;
+const int tpin = 4;
 boolean tstate = 0;
 //int upin = 13;
 //boolean ustate = 0;
-int vpin = 12;
+const int vpin = 12;
 boolean vstate = 0;
-int swpin = 7;
+const int swpin = 7;
 boolean swstate = 0;
-
-
 //PID variable definition
-float kp = 1;
+float kp = 1.0;
 float ki = 0.0;
 float kd = 0.0;
 int PID_p = 0;
 int PID_i = 0;
 int PID_d = 0;
-float last_kp = 0;
-float last_ki = 0;
-float last_kd = 0;
-float PID_Value = 0;
+float last_kp = 0.0;
+float last_ki = 0.0;
+float last_kd = 0.0;
+float PID_Value = 0.0;
 int PID_values_fixed = 0;
 float PIDError = 0;
-float previous_error = 0;
-float PreviousError = 0;
-float ElapsedTime;
+float previous_error = 0.0;
+float PreviousError = 0.0;
+float ElapsedTime = 1.0;
 float Time;
 float TimePrev;
 int PID_out = 0;
-
 //Sin wave variable definition
 float sinVal[24] = {1.0000, 0.9829, 0.9330, 0.8536, 0.7500, 0.6294, 0.5000, 0.3706, 0.2500, 0.1464, 0.0670, 0.0170, 0.0000, 0.0170, 0.0670, 0.1464, 0.2500, 0.3706, 0.5000, 0.6294, 0.7500, 0.8536, 0.9330, 0.9830};
 float ScaledSinVal = 0.0;
 int sinId = 0;
 long millisComp = 250;
 float SinScale = 712.0;
-
 #define ENCODER_A 2
 #define ENCODER_B 3
 #define MOTORPIN 11
-
-
-
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 20 x 4 line display
 Encoder myEnc(ENCODER_A, ENCODER_B);
-
-
 void setup()
 {
 //Interface pin config
@@ -90,35 +96,25 @@ pinMode(swpin, INPUT_PULLUP);
 pinMode(A3, INPUT); // analog
 pinMode(LED_BUILTIN, OUTPUT);
 pinMode(MOTORPIN, OUTPUT);
-
 //TIMER2 SETUP
 TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
 TCCR2B = _BV(CS22)|_BV(CS21)  ;
-
-
 Serial.begin(115200);
 Serial.println("Venty Startup");
-
-
 analogReference(DEFAULT);
-
 // initialize the lcd 
   lcd.init();
   lcd.backlight();
 //lcd.noBacklight();
 }
-
 void loop()
 {
     //encoder position set
     newPosition = myEnc.read();
     long encspeed = newPosition-oldPosition;
-
     mtime = millis();
-
     //set old position for next scan 
     oldPosition = newPosition;
-
 //interface pins read and states
     swstate=digitalRead(swpin); // just printed right now -Phil
     mstate=digitalRead(mpin);  // unused right now -Phil
@@ -141,64 +137,21 @@ void loop()
     vstate=digitalRead(vpin);
     if(nstate && !digitalRead(npin)){bpm --;}
     nstate=digitalRead(npin);
-
+ 
 //case output function call
 //  caseFunc();
-
 //sin output function call
   sinfunc01();
-
 //PID loop function call
   PIDloop();
-
-//LCD layout original funtion call
-//  VentyLcdOld();
-
 //New LCD layout function call
   VentyLcdNew();
-
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
   // map it to the range of the analog out:
   outputValue = map(sensorValue, 0, 1023, 64, 128);
   // change the analog out value:
   //analogWrite(analogOutPin, outputValue);
-
-//  Serial.print("sensor = ");
-//  Serial.print(sensorValue);
-//  Serial.print("\t output = ");
-//  Serial.print(outputValue);
-
-//  Serial.print("\t Speed = ");
-//  Serial.print(speed);
-//  Serial.print("\t Case = ");
-//  Serial.print(caseNum);
-//  Serial.print("\t Tick = ");
-//  Serial.print(tick);
-//  Serial.print("\t BPM = ");
-//  Serial.print(bpm);
-//  Serial.print("\t IE Ratio = ");
-//  Serial.print(ieratio);
-//  Serial.print("\t Tidal Vol = ");
-//  Serial.print(tidalvol);
-//  Serial.print("\t vPin = ");
-//  Serial.print(vstate);
-//  Serial.print("\t uPin = ");
-//  Serial.print(ustate);
-//  Serial.print("\t tPin = ");
-//  Serial.print(tstate);
-//  Serial.print("\t sPin = ");
-//  Serial.print(sstate);
-//  Serial.print("\t nPin = ");
-//  Serial.print(nstate);
-//  Serial.print("\t mPin = ");
-//  Serial.print(mstate);
-//  Serial.print("\t lPin = ");
-//  Serial.print(lstate);
-//  Serial.print("\t kPin = ");
-//  Serial.print(kstate);
-//  Serial.print("\t Time = ");
-//  Serial.print(mtime);
   Serial.print("millisComp = ");
   Serial.print(millisComp);
   Serial.print("\t sinId = ");
@@ -225,32 +178,24 @@ void loop()
   Serial.print(swstate);
   
   Serial.println();
-
   tick++;
-
   if (tick%50 == 0) {
     
 //    lcd.clear(); //Clears the LCD of previous writes
-
 //    lcd.setCursor(0,0);  
 //    lcd.print("Venty  C=  S=   ");
 //    lcd.setCursor(0,1);    
 //    lcd.print("Pot=    Enc=    ");
-
   }
-
 //case controlled output
 // analogWrite(MOTORPIN,speed); 
-
 //potentiometer controlled output
 //analogWrite(MOTORPIN,outputValue);
-
 //PID controlled output
   analogWrite(MOTORPIN,PID_out);
   
   delay(5);
 }
-
 //Sine wave stage funtion 
 void sinfunc01 (){
   if (millis() < millisComp){
@@ -265,7 +210,6 @@ void sinfunc01 (){
   }
   ScaledSinVal = sinVal[sinId] * SinScale;
 }
-
 //PID loop function
 void PIDloop(){
   TimePrev = Time;
@@ -277,30 +221,6 @@ void PIDloop(){
   PID_Value = PID_p + PID_i + PID_d;
   PID_out = map(PID_Value, 0, SinScale, 64, 128);
 }
-
-
-//OLD LCD layout
-void VentyLcdOld(){
-    lcd.setCursor(0,0);
-    lcd.print("Venty");
-    lcd.setCursor(7,0);
-    lcd.print("C=");
-    lcd.print(caseNum);
-    lcd.print(" ");
-    lcd.setCursor(11,0);
-    lcd.print("S=");
-    lcd.print(speed);
-    lcd.print(" ");
-    lcd.setCursor(0,1);
-    lcd.print("Pot=");    
-    lcd.print(sensorValue);
-    lcd.print(" ");
-    lcd.setCursor(8,1);
-    lcd.print("Enc=");    
-    lcd.print(newPosition);
-    lcd.print("   ");
-}
-
 //New LCD layout
 void VentyLcdNew(){
     lcd.setCursor(0,0);  
@@ -319,19 +239,12 @@ void VentyLcdNew(){
     lcd.print("1:");
     lcd.print(ieratio);
 }
-
-
-
-
 //Case based output function
 void caseFunc(){
-
  if (++prestate >= 2) {
     prestate = 0;
-
  digitalWrite(LED_BUILTIN,toggle4);
  toggle4 = !toggle4;
-
   state++;
   switch ((state/256) % 8) {
   case 0:
@@ -375,5 +288,4 @@ void caseFunc(){
   break;
   }
  }
-
 }
